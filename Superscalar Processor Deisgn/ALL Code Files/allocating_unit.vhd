f822493f9b5f905897e8b8d599b07b7629f053db
Ayush Agrawal <ayush.agrawal1612@gmail.com>
2017-05-14 21:43:41 +0530

    Done with 'Allocating unit'

--- Superscalar Processor Deisgn/ALL Code Files/allocating_unit.vhd
+++ Superscalar Processor Deisgn/ALL Code Files/allocating_unit.vhd
@@ -7,7 +7,7 @@ use work.components.all;
 
 
 entity allocating_unit is
-		generic(N : integer := 4;				-- Number of registers in the reservation station
+		generic(N : integer := 4;				-- Number of registers in the reservation station = Number of entries in the Free Queue
 				  X : integer := 62);			-- Size of each register
 		port(reset : in std_logic;
 			  clk : in std_logic;
@@ -23,7 +23,9 @@ entity allocating_unit is
 			  
 			  -- FROM DISPATCHING UNIT
 			  index_allocate : in main_array(0 to 1)(natural(log2(real(N)))-1 downto 0);
+					-- Indicates the indices of the freed entries in the RS
 			  valid_allocate : in main_array(0 to 1)(0 downto 0)
+					--Indicates the validity of the above data
 			  );
 	end entity;
 
@@ -31,7 +33,7 @@ architecture AU of allocating_unit is
 	
 	signal index_in,index_out : main_array(0 to N-1)(natural(log2(real(N)))-1 downto 0);
 	signal index_en,valid_in,valid_out,valid_en : main_array(0 to N-1)(0 downto 0);
-	signal top_in,top_out : std_logic_vector(natural(log2(real(N)))-1 downto 0);
+	signal top_in,top_out,top_add,top_add_one : std_logic_vector(natural(log2(real(N)))-1 downto 0);
 	signal bottom_in,bottom_out,bottom_out_add,bottom_add : std_logic_vector(natural(log2(real(N)))-1 downto 0);
 	signal stall,not_stall : std_logic;
 	
@@ -43,13 +45,13 @@ begin
 	GEN_REG : for I in 0 to N-1
 				 generate
 				 BUFF_INDEX : registers generic map(N => natural(log2(real(N))))
-												port map (reset => reset,
+												port map (reset => '0',
 															 clk	 => clk,
 															 input => index_in(I),
 															 output=> index_out(I),
 															 enable=> index_en(I)(0));
 				 BUFF_VALID : registers generic map(N => 1)
-												port map (reset => reset,
+												port map (reset => '0',
 															 clk	 => clk,
 															 input => valid_in(I),
 															 output=> valid_out(I),
@@ -78,7 +80,15 @@ begin
 																						 data2 => bottom_add,
 																						 output => bottom_in);
 	
-	process(valid_out,inst1,index_out,bottom_out_add,inst2,bottom_in,not_stall)
+	adder2 : adds generic map(N => natural(log2(real(N)))) port map(data1 => top_out,
+																						 data2 => top_add,
+																						 output => top_in);
+																						 
+	adder3 : adds generic map(N => natural(log2(real(N)))) port map(data1 => bottom_out,
+																						 data2 => (0 => '1',others => '0'),
+																						 output => top_add_one);																					 
+	
+	process(valid_out,inst1,index_out,bottom_out_add,inst2,bottom_in,not_stall,reset,valid_allocate,index_allocate,top_add_one,top_in,index_in,index_en,valid_in,valid_en)
 		variable count : integer;
 	begin
 		count := 0;
@@ -106,16 +116,70 @@ begin
 		end if;
 		
 		if(inst2(62) = '1') then
-			bottom_add <= (1 => '1', others => '0');
+			bottom_add <= (0 =>  not inst1(0), 1 => inst1(0), others => '0');
 			reg_data(to_integer(unsigned(index_out(to_integer(unsigned(bottom_in)))))) <= inst1;
 			reg_en(to_integer(unsigned(index_out(to_integer(unsigned(bottom_in)))))) <= "1";
 		else
-			bottom_add <= (0 => not_stall, others => '0');
+			bottom_add <= (0 => inst1(0), others => '0');
 		end if;
 		
 		reg_data <= (others => (others => '0')); 
 		reg_en <= (others => (others => '0')); 
 		
+		
+		-- FOR INITIALIZING THE INDEX OF THE 'FREE QUEUE' : Using Dispatching Data
+		if (reset = '1') then
+			for I in 0 to N-1 loop
+				index_in(I) <= std_logic_vector(to_unsigned(I,natural(log2(real(N)))));
+				index_en(I)(0) <= '1';
+				valid_in(I)(0) <= '1';
+				valid_en(I)(0) <= '1';
+			end loop;
+			top_add <= (others => '0');
+		else
+			-- General update policy: Using Dispatching data
+			if (valid_allocate(0)(0) = '1' and valid_allocate(1)(0) = '1') then		-- Indicates 2 instructions are dispatched
+				top_add <= (1 => '1', others => '0');
+				index_in(to_integer(unsigned(top_add_one))) <= index_allocate(0);
+				index_en(to_integer(unsigned(top_add_one)))(0) <= '1';
+				index_in(to_integer(unsigned(top_in))) <= index_allocate(1);
+				index_en(to_integer(unsigned(top_in)))(0) <= '1';
+				index_in <= (others => (others => '0'));
+				index_en <= (others => (others => '0'));
+				valid_in(to_integer(unsigned(top_add_one)))(0) <= '1';
+				valid_en(to_integer(unsigned(top_add_one)))(0) <= '1';
+				valid_in(to_integer(unsigned(top_in)))(0) <= '1';
+				valid_en(to_integer(unsigned(top_in)))(0) <= '1';
+				valid_in <= (others => (others => '0'));
+				valid_en <= (others => (others => '0'));
+			else
+				top_add <= (0 => valid_allocate(0)(0) and valid_allocate(1)(0), others => '0');
+				if (valid_allocate(0)(0) = '1') then
+					index_in(to_integer(unsigned(top_add_one))) <= index_allocate(0);
+					index_en(to_integer(unsigned(top_add_one)))(0) <= '1';
+					index_in <= (others => (others => '0'));
+					index_en <= (others => (others => '0'));
+					valid_in(to_integer(unsigned(top_add_one)))(0) <= '1';
+					valid_en(to_integer(unsigned(top_add_one)))(0) <= '1';
+					valid_in <= (others => (others => '0'));
+					valid_en <= (others => (others => '0'));
+				elsif(valid_allocate(1)(0) = '1') then
+					index_in(to_integer(unsigned(top_add_one))) <= index_allocate(1);
+					index_en(to_integer(unsigned(top_add_one)))(0) <= '1';
+					index_in <= (others => (others => '0'));
+					index_en <= (others => (others => '0'));
+					valid_in(to_integer(unsigned(top_add_one)))(0) <= '1';
+					valid_en(to_integer(unsigned(top_add_one)))(0) <= '1';
+					valid_in <= (others => (others => '0'));
+					valid_en <= (others => (others => '0'));
+				else
+					index_in <= (others => (others => '0'));
+					index_en <= (others => (others => '0'));
+					valid_in <= (others => (others => '0'));
+					valid_en <= (others => (others => '0'));
+				end if;
+			end if;			
+		end if;
 	end process;
 	
 end;
